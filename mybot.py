2838fa0e04a21d23b656ce620f78c1c6071f22fe
infopz <casari.giovanni@gmail.com>
2016-09-20 23:19:45 +0200

    Added Data and Time
    Added date and time to /voto command (now calls /ds).
    th txt file name is changed to CoseSalvate

--- mybot.py
+++ mybot.py
@@ -1,24 +1,30 @@
 import botogram
 import requests
+import datetime
+
+'''mettere bagaglio due cifre mese e anno
+   provare ad aggiustare roba prima riga'''
 
 bot = botogram.create("290577957:AAGu15bceNMkGllsC8Hk7M6AhE2vbkItfCE")
 bot.about="Questo è il bot di Infopz"
 bot.owner="@infopz"
 
-text_file = open("Voti.txt", "w")
+text_file = open("CoseSalvate.txt", "a")
 
 @bot.command("hello")
 def hello_command(chat, message, args):
     chat.send("Hello World")
 
-@bot.command("voto")
-def voto_command(chat, message, args):
-    a=message.text
-    a=a[6:]
-    a= a + "\n"
-    text_file.write(a)
+@bot.command("ds") #salva messaggio ricevuto con ora e data
+def save_command(chat, message, args):
+    mr=message.text
+    mr=mr[4:]
+    i=datetime.datetime.now()
+    dt=str(i.year)+"."+str(i.month)+"."+str(i.day)+"."+str(i.hour)+"."+str(i.minute)
+    ms="\r"+dt+"_"+mr
+    text_file.write(ms)
     text_file.close()
-    chat.send("Messaggio "+a+" salvato correttamente!")
+    chat.send("Messaggio "+mr+" salvato correttamente!")
 
 @bot.message_contains("infopz")
 def send_botogram_link(chat, message):
