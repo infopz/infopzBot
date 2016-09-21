45f4ebcbbc0995fdd67ca32d23987b724cb169f1
infopz <casari.giovanni@gmail.com>
2016-09-21 19:05:43 +0200

    Resolve problem and Small changes
    Resolve date and time problem
    Changed time library (from datetime to time)
    Add Cit message when use /ds command

--- mybot.py
+++ mybot.py
@@ -1,9 +1,15 @@
 import botogram
 import requests
 import datetime
+import time
 
-'''mettere bagaglio due cifre mese e anno
-   provare ad aggiustare roba prima riga'''
+'''comando Read
+   cambio Euro-Dollaro'''
+
+def dataTempo():
+   current_time = time.localtime()
+   dt=time.strftime('%Y.%m.%d,%H.%M', current_time)
+   return dt
 
 bot = botogram.create("290577957:AAGu15bceNMkGllsC8Hk7M6AhE2vbkItfCE")
 bot.about="Questo è il bot di Infopz"
@@ -19,12 +25,11 @@ def hello_command(chat, message, args):
 def save_command(chat, message, args):
     mr=message.text
     mr=mr[4:]
-    i=datetime.datetime.now()
-    dt=str(i.year)+"."+str(i.month)+"."+str(i.day)+"."+str(i.hour)+"."+str(i.minute)
-    ms="\r"+dt+"_"+mr
+    dt=dataTempo()
+    ms=dt+" _ "+mr+"\n"
     text_file.write(ms)
     text_file.close()
-    chat.send("Messaggio "+mr+" salvato correttamente!")
+    message.reply("Messaggio Salvato!", preview=True, syntax=None, extra=None, notify=True)
 
 @bot.message_contains("infopz")
 def send_botogram_link(chat, message):
@@ -38,5 +43,6 @@ def apple_links(chat, message, matches):
 
 if __name__ == "__main__":
     bot.run()
+
 
 text_file.close()
