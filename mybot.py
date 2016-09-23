8277c7d7700babd935d33622c65bd0cd3133b4a1
infopz <casari.giovanni@gmail.com>
2016-09-23 16:26:34 +0200

    Added the /cambio command
    Also added the func CalcolaCambio.
    Finished writing the commands sentences for the help.

--- mybot.py
+++ mybot.py
@@ -2,30 +2,34 @@ import botogram
 import requests
 import datetime
 import time
-
-#cambio Euro-Dollaro
+from urllib.request import urlopen
 
 def dataTempo():
    current_time = time.localtime()
    dt=time.strftime('%Y.%m.%d,%H.%M', current_time)
    return dt
 
+def ottieniCambio():
+   data=urlopen("http://dollaro-euro.it/")
+   page= str(data.read())
+   page=page[3035:]
+   page=float(page[0:6])
+   return(page)
+
 bot = botogram.create("290577957:AAGu15bceNMkGllsC8Hk7M6AhE2vbkItfCE")
 bot.about="Questo è il bot di Infopz"
 bot.owner="@infopz"
 
-
-
 @bot.command("hello")
 def hello_command(chat, message, args):
     '''Ciaone!
-       Ti saluta con un bel ciao'''
+       \nTi saluta con un bel ciao'''
     chat.send("Hello World")
 
 @bot.command("ds") #salva messaggio ricevuto con ora e data
 def save_command(chat, message, args):
     '''Salva il messaggio
-       Questo comando salva in un file txt il messaggio indicando a fianco data e ora'''
+       \nQuesto comando salva in un file txt il messaggio indicando a fianco data e ora'''
     file = open("CoseSalvate.txt", "a")  
     mr=message.text
     mr=mr[4:]
@@ -38,7 +42,7 @@ def save_command(chat, message, args):
 @bot.command("leggi")
 def leggi_command(chat, message, args):
     '''Legge le cose salvate
-    Legge n righe a partire dal fondo nel file CoseSalvate.txt, se non vengono forniti legge l'intero file'''
+    \nLegge n righe a partire dal fondo nel file CoseSalvate.txt, se non vengono forniti legge l'intero file'''
     file = open("CoseSalvate.txt", "r")
     lines=file.readlines()
     n=message.text
@@ -53,7 +57,27 @@ def leggi_command(chat, message, args):
       for line in lines[(m-n):m]:
         risp=risp+line
       chat.send(risp[:-1])
-      
+
+@bot.command("cambio")
+def cambio_command(chat, message, args):
+    '''Cambia dollari in euro e viceversa
+       \nCambia i Dollari in euro e Euro in dollari in base al simbolo di valuta messo dopo il numero. \nSe viene fornita la parola Tasso viene fornito il tasso di cambio €/$'''
+    cambio=ottieniCambio()
+    n=message.text
+    n=n[8:]
+    if n=='tasso':
+       ris="Il tasso di oggi è "+str(cambio)+" €/$"
+    elif (n[-1:])=='€':
+       n=float(n[:-1])
+       ris=round(float(n/cambio), 3)
+       ris=str(n)+"€ sono "+str(ris)+"$"
+    elif (n[-1:])=='$':
+       n=float(n[:-1])
+       ris=round(float(n*cambio), 3)
+       ris=str(n)+"$ sono "+str(ris)+"€"
+    else:
+       ris="Valuta inserita non corretta o non data\nInserici un valore con € o $ alla fine"
+    chat.send(ris)
 
 @bot.message_contains("infopz")
 def send_botogram_link(chat, message):
